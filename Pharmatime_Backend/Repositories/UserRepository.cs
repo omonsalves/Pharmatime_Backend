03fe91a0d20905817c9cd6fca42f41e6ce1b2c2c
ORLANDO MONSALVE SERRATO <omonsalves@ucundinamarca.edu.co>
2024-03-29 11:22:49 -0500

    User repository

--- Pharmatime_Backend/Repositories/UserRepository.cs
+++ Pharmatime_Backend/Repositories/UserRepository.cs
@@ -18,7 +18,7 @@ public class UserRepository
 
                 if(usuario == null) {
                     var user = new Usuario()
-                    {
+                    { 
 
                         Nombre = model.Nombre,
                         Apellido = model.Apellido,
